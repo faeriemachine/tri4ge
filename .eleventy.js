1e5e5c332970338523e967406c1db9013a22a3e7
Triage <thepallascat@gmail.com>
2026-02-22 15:45:39 -0500

    Attempt to fix tag rendering

--- .eleventy.js
+++ .eleventy.js
@@ -27,6 +27,7 @@ module.exports = function (eleventyConfig) {
     });
     eleventyConfig.addFilter("findTagCount", (tagList, findTag) => tagList.find(({ tag }) => tag === findTag)?.count);
 
+// Word Count //
   const wordcountPlugin = require("eleventy-plugin-wordcount-extended");
   module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(wordcountPlugin);
