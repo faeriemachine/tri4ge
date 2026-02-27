d55bd22edf1a60ce212512398338e98feb06938d
Triage <thepallascat@gmail.com>
2026-02-27 16:13:21 -0500

    Add preloader

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,5 +1,5 @@
 	$(window).load(function() {
 		$('.lds-heart').delay(400).fadeOut('slow');
     		  $('.mask').delay(300).fadeOut('slow'); 
-		$('#loaded').delay(300).css({'overflow':'visible'});
+		$('body').delay(300).css({'overflow':'visible'});
 	  });
