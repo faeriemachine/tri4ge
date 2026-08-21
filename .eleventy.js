43e72e7875408e0f1325aadacad3243e6579905b
Triage <thepallascat@gmail.com>
2026-08-21 15:49:35 -0400

    Add new art + bug fixes + add sites to links

--- .eleventy.js
+++ .eleventy.js
@@ -5,10 +5,20 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/assets");
   eleventyConfig.addFilter("date", require("./src/filters/date.js"));
-  
+
   eleventyConfig.addCollection('posts', function(collectionApi) {
     return collectionApi.getFilteredByGlob('src/blog/posts/**/*.md');
 
+  });
+
+  eleventyConfig.addCollection('recentPosts', function(collectionApi) {
+    return collectionApi.getFilteredByGlob('src/blog/posts/**/*.md').reverse().slice(0, 5);
+
+  });
+
+
+  eleventyConfig.addCollection("recentArt", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/gallery/*.md").reverse().slice(0, 10);
   });
 
   // Exclude certain tags from displaying
@@ -31,7 +41,7 @@ module.exports = function (eleventyConfig) {
   module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(wordcountPlugin);
   };
-  
+
     return {
       passthroughFileCopy: true,
       dir: {
